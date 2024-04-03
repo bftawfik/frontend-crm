745498ca9c267fead82b55c65d2786e0d7642e7c
Mennatullah Ibraheem <34405649+MennaTallah2@users.noreply.github.com>
2024-04-03 14:00:59 +0200

    project main layout

---
+++ app/_components/svgs/MenuIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+export const MenuIcon: React.FC<SvgProps> = ({ width = 24, height = 24, className = "h-6 w-6" }) => {
+  return (
+    <svg
+      className={className}
+      width={width}
+      height={height}
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        clip-rule="evenodd"
+        fill-rule="evenodd"
+        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+      ></path>
+    </svg>
+  )
+}
